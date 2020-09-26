bc35f35a52efa5a08a72704ad25e875253c44ddc
Vasanthan <vasanth2754@gmail.com>
2020-09-26 09:05:51 +0530

    Updates

---
+++ precache-manifest.9cc38bd065cf799c994dad1727ea1705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ab3e1fd1b573b2b7b4a2801fa60d9f",
+    "url": "/Food-Recipe-App/index.html"
+  },
+  {
+    "revision": "ed2c7cd04112c1673952",
+    "url": "/Food-Recipe-App/static/css/main.19fcb500.chunk.css"
+  },
+  {
+    "revision": "6d67894d5ab4d3145ad6",
+    "url": "/Food-Recipe-App/static/js/2.20dedf64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipe-App/static/js/2.20dedf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2c7cd04112c1673952",
+    "url": "/Food-Recipe-App/static/js/main.a7b805ab.chunk.js"
+  },
+  {
+    "revision": "1deeda1f3b0b620a96b9",
+    "url": "/Food-Recipe-App/static/js/runtime-main.5ab5f08b.js"
+  }
+]);
